e527590f4d7cfe459c348e8d5ef3624849c4b249
hivemkt <email.brunoalbuquerque@gmail.com>
2026-02-08 20:43:20 -0300

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,10 +1,13 @@
 exports.handler = async () => {
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
     body: JSON.stringify({
       SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-      MERCADOPAGO_PUBLIC_KEY: process.env.MERCADOPAGO_PUBLIC_KEY
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
     })
   };
 };
